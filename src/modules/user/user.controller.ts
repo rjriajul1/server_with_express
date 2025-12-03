b96531aaac7abada00c262ee507f983afbdf0dfa
Riajul Islam <rjriajulislam9@gmail.com>
2025-12-04 03:44:27 +0600

    all user converted modules partter

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userServices } from "./user.service";
+import { pool } from "../../config/db";
 
 const createUser = async (req: Request, res: Response) => {
   const { name, email } = req.body;
@@ -20,7 +21,115 @@ const createUser = async (req: Request, res: Response) => {
   }
 }
 
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getUser()
+    res.status(200).json({
+      success: true,
+      message: "users retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+}
+
+const getSingleUser = async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+
+  try {
+    const result = await userServices.getSingleUser(req.params.id as string);
+    
+    if (result.rows.length === 0){
+      res.status(404).json({
+          success: false,
+          message: "User not found",
+        })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User fetched successfully",
+        data: result.rows[0]
+      })
+    }
+     
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+}
+
+const deleteUser = async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+
+  try {
+    const result = await userServices.deleteUser(req.params.id as string) ;
+
+    // console.log(result);
+
+    if (result.rowCount === 0){
+      res.status(404).json({
+          success: false,
+          message: "User not found",
+        })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        data: result.rows
+      })
+    }
+     
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+
+  const {name, email} = req.body;
+
+  try {
+    const result = await userServices.updateUser(name,email,req.params.id!);
+
+    if (result.rows.length === 0){
+      res.status(404).json({
+          success: false,
+          message: "User not found",
+        })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0]
+      })
+    }
+     
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+}
+
 export const userControllers = {
     createUser,
+    getUser,
+    getSingleUser,
+    deleteUser,
+    updateUser
 
 }
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-import { pool } from "../../config/db";
+import express from "express";
+// import { pool } from "../../config/db";
 import { userControllers } from "./user.controller";
 
 const router = express.Router();
@@ -7,23 +7,10 @@ const router = express.Router();
 
 // app.use("/users", userRoutes)
 //router -> controller -> service
-router.post("/", userControllers.createUser)
-
-router.get('/',  async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`SELECT * FROM users`);
-    res.status(200).json({
-      success: true,
-      message: "users retrived successfully",
-      data: result.rows,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-      datails: err,
-    });
-  }
-})
+router.post("/", userControllers.createUser);
+router.get('/',  userControllers.getUser);
+router.get('/:id', userControllers.getSingleUser);
+router.delete('/:id', userControllers.deleteUser);
+router.put('/:id', userControllers.updateUser);
 
 export const userRoute = router;
