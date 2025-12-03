b7c26e29ba4184cbe48e67cc12c3103ddf3e74f7
Riajul Islam <rjriajulislam9@gmail.com>
2025-12-04 01:54:16 +0600

    create user.service and clearn user.controller

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { userServices } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await userServices.createUser(name,email)
+    // console.log(result.rows[0]);
+    // res.send({ message: "data inserted" });
+    res.status(201).json({
+      success: true,
+      message: "data inserted successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+}
+
+export const userControllers = {
+    createUser,
+
+}
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,31 +1,13 @@
 import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
+import { userControllers } from "./user.controller";
 
 const router = express.Router();
 
 
 // app.use("/users", userRoutes)
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  try {
-    const result = await pool.query(
-      `INSERT INTO users(name, email) VALUES($1,$2) RETURNING *`,
-      [name, email]
-    );
-    // console.log(result.rows[0]);
-    // res.send({ message: "data inserted" });
-    res.status(201).json({
-      success: true,
-      message: "data inserted successfully",
-      data: result.rows[0],
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-})
+//router -> controller -> service
+router.post("/", userControllers.createUser)
 
 router.get('/',  async (req: Request, res: Response) => {
   try {
