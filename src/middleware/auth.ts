05579aebad1c003824ba2c05ac2780267eaf8701
Riajul Islam <rjriajulislam9@gmail.com>
2025-12-06 22:44:21 +0600

    implement auth and complete any process

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,22 +1,28 @@
-import { NextFunction, Request, Response } from "express"
-import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 const auth = () => {
-    return (req: Request, res: Response, next:NextFunction) => {
-         const token = req.headers.authorization;
-         console.log("auth token", token);
-         if(!token){
-            return res.status(500).json({
-                message: "You are not allowed!!"
-            })
-         }
-     const decoded = jwt.verify(token, config.jwtSecret as string)
-     console.log(decoded);
-      next()
-    }
-    
-}
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
 
+      if (!token) {
+        return res.status(500).json({
+          message: "You are not allowed!!",
+        });
+      }
+      const decoded = jwt.verify(token, config.jwtSecret as string);
+      //  console.log(decoded);
+      req.user = decoded as JwtPayload;
+      next();
+    } catch (err: any) {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+};
 
 export default auth;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,7 +6,7 @@ const loginUser = async (email: string, password: string) => {
   const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [
     email,
   ]);
-  console.log("result", result);
+  // console.log("result", result);
   if (result.rows.length === 0) {
     return null;
   }
